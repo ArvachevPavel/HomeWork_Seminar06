0126c59b0afbd404b3dce3c55636b970ce09c191
Pavel Arvachev <110897986+ArvachevPavel@users.noreply.github.com>
2022-09-07 15:29:38 +0300

    Add files via upload

---
+++ Seminar06/Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите, какое количество чисел вам надо сравнить");
+int QuantityOfNumbers = int.Parse(Console.ReadLine());
+int i = 0;
+int count = 0;
+string b = "";
+while (i < QuantityOfNumbers)
+{
+    Console.WriteLine("Введите число ");
+    int a = int.Parse(Console.ReadLine());
+    if (a > 0)
+    {
+        count = count + 1;        
+    }
+    i++;
+    b = b  + " " + a + ",";
+       
+}
+Console.Write(b); 
+Console.Write("-> " + count);
+
